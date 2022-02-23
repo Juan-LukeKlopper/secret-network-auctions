a0dbf76ff2a1f990958f2ff088147f7bd791a1b8
Juan-LukeKlopper <85296853+Juan-LukeKlopper@users.noreply.github.com>
2022-02-23 23:46:31 +0200

    added griptape and basic config

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
-import App from './App.svelte'
+import App from './App.svelte';
+import {
+	gripApp,
+	getKeplrAccountProvider
+} from '@stakeordie/griptape.js';
+  
+const restUrl = 'https://api.pulsar.griptapejs.com';
+const provider = getKeplrAccountProvider();
+let app
+function runApp() {
+	app = new App({
+		target: document.getElementById('app')
+	  })
+};
 
-const app = new App({
-  target: document.getElementById('app')
-})
+gripApp(restUrl, provider, runApp);
 
 export default app
