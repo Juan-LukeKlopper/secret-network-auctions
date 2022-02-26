a2d5a15868f9e38b1cbcb397282b9818271e5415
Juan-LukeKlopper <85296853+Juan-LukeKlopper@users.noreply.github.com>
2022-02-26 16:33:00 +0200

    Changed account info section into a component

---
+++ src/components/Account.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import {
+        getNativeCoinBalance,
+        coinConvert,
+        getAddress,
+    } from "@stakeordie/griptape.js"
+
+    async function getBalance() {
+    let balance = await getNativeCoinBalance();
+    balance = coinConvert(balance, 6, "human");
+    return balance
+    }
+</script>
+
+{#await getAddress() then address}
+    <p>Your address is: {address}</p>
+{/await}
+
+{#await getBalance()}
+    <p>...waiting</p>
+{:then balance} 
+    <p>Your balance is : {balance} </p>
+{/await}
+
