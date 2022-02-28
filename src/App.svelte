bf2661574b045befe132d1d7c5554492cbb55a25
Juan-LukeKlopper <85296853+Juan-LukeKlopper@users.noreply.github.com>
2022-02-28 23:06:26 +0200

    added wallet ,moved auctions into custom component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { onMount } from "svelte";
   import Navbar from "./components/Navbar.svelte";
   import style from "./styling.svelte";
   import { auctionFactory } from "./contracts/auction-factory";
@@ -13,13 +12,14 @@
     refContract,
   } from "@stakeordie/griptape.js";
   import { auctionDef } from "./contracts/auction";
-  import type { Auction } from "./contracts/types";
-  import { loop_guard } from "svelte/internal";
   import Account from "./components/Account.svelte";
+  import Wallet from "./components/Wallet.svelte";
+  import Auctions from "./components/Auctions.svelte";
 
   let activeAuctions = [];
-  let auction = "";
+  let auctiondetails = "";
   let connected = false;
+  export let showWallet = false;
 
   function initAuctions(activeAuctions) {
     activeAuctions.forEach((item) =>
@@ -42,47 +42,31 @@
   async function getAuctionInfo(address) {
     const auctionContract = refContract(address);
     const { auction_info: result } = await auctionContract.getAuctionInfo();
-    auction = result;
+    auctiondetails = result;
+    showWallet = false;
   }
 
   onAccountAvailable(() => {
     connected = true;
   });
-
-  onMount(() => {
-    listActiveAuction();
-  });
 </script>
 
 <main>
   <Navbar />
-  
 
   {#if !connected}
     <h1>Welcome to The secret network auction house!</h1>
+    <p>Please connect your wallet to continue.</p>
     <button on:click={bootstrap}> connect </button>
   {:else}
-      <Account />
-  {/if}
-
-  <p>Active auctions</p>
-
-  {#each activeAuctions as auction}
-    <li>
-      {auction.label}
-      <button on:click={() => getAuctionInfo(auction.address)}
-        >get more info</button>
-    </li>
-  {/each}
-
-  {#if auction != ""}
-    <p>
-      {auction.sell_token.token_info.symbol} - {auction.bid_token.token_info.symbol} auction
-    </p>
-    <p>Auction address: {auction.auction_address}</p>
-    <p>description: {auction.description}</p>
-    <p>Closes at: {auction.ends_at}</p>
-
-    {console.log(auction.status.toString())}
+    <Account />
+    {#if !showWallet}
+      <button on:click={() => (showWallet = !showWallet)}> show wallet </button>
+    {:else}
+      <button on:click={() => (showWallet = !showWallet)}> hide wallet </button>
+      <Wallet/>
+      {auctiondetails = ""}
+    {/if}
+    <Auctions/>
   {/if}
 </main>
