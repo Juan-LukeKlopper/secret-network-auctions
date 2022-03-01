49b82cfdf468e91220eefd1e08f3c310554b21b1
Juan-LukeKlopper <85296853+Juan-LukeKlopper@users.noreply.github.com>
2022-03-01 09:09:59 +0200

    Added more stores and a message in auction contract

---
+++ src/components/CreateAuction.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+    import { createContractClient, refContract, snip20Def } from "@stakeordie/griptape.js";
+    import { auctionFactory } from "../contracts/auction-factory";
+    import tokens from "../data/tokens.json"
+
+    const form = ({
+        sellAmount: null,
+        minimumBid: null,
+        endsAt: null,
+        description: null,
+        sellContract: null,
+        bidContract: null,
+    })
+    let bid_token,sell_token, unixdate ;
+    let sell_amount,  min_bid, ends_at, description;
+
+    async function createAuction() {
+       // const result = 
+
+        unixdate = Date.parse(ends_at);
+        form.sellAmount = sell_amount;
+        form.minimumBid= min_bid;
+        form.endsAt= ends_at ;
+        form.description= description;
+        form.sellContract= sell_token;
+        form.bidContract= bid_token;
+
+        const contract = refContract(sell_token);
+        try {
+            console.log(unixdate)
+            await contract.increaseAllowences(auctionFactory.at, sell_amount);
+            await auctionFactory.createAuction(form)
+            
+        } finally {
+            
+        }
+        
+    }
+
+    function createSellContractClient() {
+        const address = {sell_token};
+        if (!address) return;
+        createContractClient({
+            id: address,
+            at: address,
+            definition: snip20Def
+        }); 
+    }
+</script>
+
+<div class="auctions">
+    <p>Create a custom auction</p>
+
+    <p>Sell contract:</p>
+    <select bind:value={sell_token}>
+        {#each tokens as token}
+            <option value={token.address}> {token.name}</option>
+        {/each}
+    </select>
+    <input type="text" placeholder="Sell amount" bind:value={sell_amount} min=0/>
+
+    <p>Bid contract:</p>
+    <select bind:value={bid_token}>
+        {#each tokens as token}
+            <option name={bid_token} value={token.address} > {token.name} </option>
+        {/each}
+    </select>
+    <input type="text" placeholder="Minimum bid" bind:value={min_bid} min=0/>
+
+    <p>Ends at:</p>
+    <input type="datetime" bind:value={ends_at}/>
+    
+    <p>Description: (optional)</p>
+    <textarea bind:value={description}></textarea>
+
+    <button on:click={() => createAuction()}>Create Auction</button>
+</div>
+
+<style>
+    
+</style>
--- src/contracts/auction-factory.ts
+++ src/contracts/auction-factory.ts
@@ -3,11 +3,53 @@ import {
     ContractQueryRequest
   } from '@stakeordie/griptape.js';
 import type { AuctionFactory } from './types';
+import { v4 as uuidv4} from 'uuid';
+
+function getCodeHash(address) {
+  if (address === 'secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg')
+    return '9587d60b8e6b078ace12014ceeee089530b9fabcd76535d93666a6c127ad8813'
+  return 'cd306596d0c5f43d99b7085c35be615dd2210c0a36bddfb4189b6a54d0906d95'
+}
+
+const { v4: UUidv4} = require('uuid');
 
 const auctionFactoryDef = {
     queries:  {
         listActiveAuctions(): ContractQueryRequest {
           return { list_active_auctions: {} };
+      }
+    },
+    messages: {
+      createViewingKey({ entropy }) {
+        const handleMsg = {
+          create_viewing_key: {
+            entropy
+          }
+        };
+        return {handleMsg};
+      },
+      createAuction(_, form) {
+        const label = uuidv4();
+        const sellTokenCodeHash = getCodeHash(form.sellContract);
+        const bidTokenCodeHash = getCodeHash(form.bidContract);
+        const handleMsg = {
+          create_auction: {
+            label,
+            sell_contract: {
+              address: form.sellContract,
+              code_hash: sellTokenCodeHash
+            },
+            bid_contract: {
+              address: form.bidContract,
+              code_hash: bidTokenCodeHash
+            },
+            sell_amount: form.sellAmount,
+            minimum_bid: form.minimumBid,
+            ends_at: form.endsAt,
+            description: form.description
+          }
+        };
+        return {handleMsg};
       }
     }
 }
